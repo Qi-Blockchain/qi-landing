0573e5d1050c23bb692ef13ef3b2c6168a9cfb1f
Никита Тимошенко <Nikita@MacBook-Pro-Nikita.local>
2022-10-11 03:42:23 +0300

    style: add gap

--- src/pages/Home/startScreen/textSide/featuresMobile/index.jsx
+++ src/pages/Home/startScreen/textSide/featuresMobile/index.jsx
@@ -7,26 +7,28 @@ import "@egjs/react-flicking/dist/flicking.css";
 const FeaturesMobile = () => {
     return (
         <div className={classes.wrapper}>
-            <Flicking moveType="freeScroll" gap={40} bound={true}>
-                <div>
-                    <TextBlurBg text='Scalable and Developer Friendly'/>
-                </div>
-                <div>
-                    <TextBlurBg text='Secure, Fast'/>
-                </div>
-                <div>
-                    <TextBlurBg text='Eco-friendly'/>
-                </div>
-                <div>
-                    <TextBlurBg text='Decentralised'/>
-                </div>
-                <div>
-                    <TextBlurBg text='Low transaction fees'/>
-                </div>
-                <div>
-                    <TextBlurBg text='Easy Built decentralised Apps'/>
-                </div>
-            </Flicking>
+            <div className={'flick-custom'}>
+                <Flicking moveType="freeScroll" bound={true}>
+                    <div>
+                        <TextBlurBg text='Scalable and Developer Friendly'/>
+                    </div>
+                    <div>
+                        <TextBlurBg text='Secure, Fast'/>
+                    </div>
+                    <div>
+                        <TextBlurBg text='Eco-friendly'/>
+                    </div>
+                    <div>
+                        <TextBlurBg text='Decentralised'/>
+                    </div>
+                    <div>
+                        <TextBlurBg text='Low transaction fees'/>
+                    </div>
+                    <div>
+                        <TextBlurBg text='Easy Built decentralised Apps'/>
+                    </div>
+                </Flicking>
+            </div>
         </div>
     );
 };
