07374dce1a479f38ee4646952ac58c25a89eb216
Никита Тимошенко <Nikita@MacBook-Pro-Nikita.local>
2022-10-12 19:36:29 +0300

    feat: add change value percent

--- src/pages/Home/liveChartSection/numbersBlock/index.jsx
+++ src/pages/Home/liveChartSection/numbersBlock/index.jsx
@@ -4,11 +4,13 @@ import {ReactComponent as Logo} from "../../../../assets/images/logo.svg";
 
 const NumbersBlock = () => {
     const [state, setState] = useState(0);
+    const [numChange, setNumChange] = useState();
 
     const getNum = () => {
         fetch('https://solo.qi.mineradnow.space/api2/v1/token/QIE/rates?period=1h')
             .then((response) => response.json())
             .then((data) => {
+                setNumChange(data.data.rates.rateChange)
                 setState(data.data.rates.rateData[data.data.rates.rateData.length - 1].rate.toFixed(4))
             });
     }
@@ -30,7 +32,9 @@ const NumbersBlock = () => {
             </div>
             <div className={classes.priceWrapper}>
                 <div className={classes.price}>${state}</div>
-                {/*<div className={classes.label}>+4.47%</div>*/}
+                {numChange?.type === 'up' &&
+                    <div className={classes.label}>+{numChange.changePercent}%</div>
+                }
             </div>
         </div>
     );
