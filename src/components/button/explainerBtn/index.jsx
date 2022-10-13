119be07f0291fc90fe3bb940a7e2f77aa554f061
Никита Тимошенко <Nikita@MacBook-Pro-Nikita.local>
2022-10-13 15:37:37 +0300

    feat: add modal with video

--- src/components/button/explainerBtn/index.jsx
+++ src/components/button/explainerBtn/index.jsx
@@ -2,40 +2,46 @@ import React from 'react';
 import classes from './styles.module.scss';
 import {ReactComponent as PlayIcon} from "../../../assets/images/play-icon.svg";
 import Modal from 'react-modal';
+import ReactPlayer from "react-player";
+import {ReactComponent as CloseIcon} from "../../../assets/images/x-modal-icon.svg";
 
-const ExplainerBtn = ({link}) => {
+const ExplainerBtn = ({link, url}) => {
     const [modalIsOpen, setIsOpen] = React.useState(false);
 
-    function openModal() {
-        setIsOpen(true);
-    }
+    const openModal = () => setIsOpen(true);
 
-    function closeModal() {
-        setIsOpen(false);
-    }
+    const closeModal = () => setIsOpen(false);
 
     return (
         <>
-            {/*<Modal*/}
-            {/*    isOpen={modalIsOpen}*/}
-            {/*    onRequestClose={closeModal}*/}
-            {/*    contentLabel="Example Modal"*/}
-            {/*>*/}
-            {/*    <button onClick={closeModal}>close</button>*/}
-            {/*    <div>I am a modal</div>*/}
-            {/*    <form>*/}
-            {/*        <input />*/}
-            {/*        <button>tab navigation</button>*/}
-            {/*        <button>stays</button>*/}
-            {/*        <button>inside</button>*/}
-            {/*        <button>the modal</button>*/}
-            {/*    </form>*/}
-            {/*</Modal>*/}
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Example Modal"
+            >
+                <div className={classes.modalWrapper}>
+                    <div className={classes.iconWrapper} onClick={closeModal}>
+                        <CloseIcon/>
+                    </div>
+                    <ReactPlayer
+                        className='react-player'
+                        config={{
+                            youtube: {
+                                playerVars: { disablekb: 1 }
+                            }
+                        }}
+                        playIcon={<PlayIcon/>}
+                        url={'https://www.youtube.com/watch?v=1x4-YS-PzYA'}
+                        width={459}
+                        height={245}
+                    />
+                </div>
+            </Modal>
             <button className={classes.button} onClick={openModal}>
-                <a href={link} target={'_blank'}>
+                {/*<a href={link} target={'_blank'}>*/}
                     Explainer Video
                     <PlayIcon/>
-                </a>
+                {/*</a>*/}
             </button>
         </>
     );
