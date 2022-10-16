63c98d9db0de3881dd80230a821c53560315157c
Никита Тимошенко <Nikita@MacBook-Pro-Nikita.local>
2022-10-16 20:28:54 +0300

    fix: overflow fix

--- src/pages/Home/startScreen/illustrationSide/floatingText/index.jsx
+++ src/pages/Home/startScreen/illustrationSide/floatingText/index.jsx
@@ -12,11 +12,11 @@ const FloatingText = () => {
 
     return (
         <div ref={ref} id={'scene'} className={classes.wrapper} >
-            <TextBlurBg text='Scalable and Developer' secondLine='Friendly' depth={0.1}/>
-            <TextBlurBg text='Secure, Fast' depth={0.3} />
-            <TextBlurBg text='Eco-friendly' depth={0.5} />
-            <TextBlurBg text='Decentralised' depth={0.7} />
-            <TextBlurBg text='Low transaction fees' depth={0.9} />
+            <TextBlurBg text='Scalable and Developer' secondLine='Friendly' depth={1}/>
+            <TextBlurBg text='Secure, Fast' depth={1} />
+            <TextBlurBg text='Eco-friendly' depth={1} />
+            <TextBlurBg text='Decentralised' depth={1} />
+            <TextBlurBg text='Low transaction fees' depth={1} />
             <TextBlurBg text='Easy Built' secondLine='decentralised Apps' depth={1} />
         </div>
     );
