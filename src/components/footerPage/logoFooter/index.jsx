f71ba9f99429d612253164380cf248a2fed6a5bb
Никита Тимошенко <Nikita@MacBook-Pro-Nikita.local>
2022-10-16 13:05:00 +0300

    style: change animation delay

--- src/components/footerPage/logoFooter/index.jsx
+++ src/components/footerPage/logoFooter/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import classes from "./styles.module.scss";
 import {ReactComponent as LogoIcon} from "../../../assets/images/logo-icon-monochrome.svg";
+import {ROUTES} from "../../../constant";
 
 const LogoFooter = () => {
     return (
-        <div className={classes.wrapper}>
+        <Link to={ROUTES.HOMEPAGE} className={classes.wrapper}>
             <div>{<LogoIcon/>}</div>
             <div className={classes.logoText}>Qi Blockchain</div>
-        </div>
+        </Link>
     );
 };
 
--- src/components/headerPage/logoHeader/index.jsx
+++ src/components/headerPage/logoHeader/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import classes from './styles.module.scss';
 import {ReactComponent as LogoIcon} from "assets/images/logo.svg";
 import {ReactComponent as LogoText} from "assets/images/logo-text.svg";
+import {Link} from "react-router-dom";
+import {ROUTES} from "../../../constant";
 
 const LogoHeader = () => {
     return (
-        <div className={classes.wrapper}>
+        <Link to={ROUTES.HOMEPAGE} className={classes.wrapper}>
             <div className={classes.logoIcon}>{<LogoIcon/>}</div>
             <div className={classes.logoText}>Qi Blockchain</div>
-        </div>
+        </Link>
     );
 };
 
