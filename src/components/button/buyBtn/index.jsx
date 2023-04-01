7d328259cfc4490460d465273561e93b1ce96ab4
Nikita Timoshenko <timoshenkonikita@gmail.com>
2023-04-01 15:05:56 +0300

    feat: add payment modal

--- src/components/button/buyBtn/index.jsx
+++ src/components/button/buyBtn/index.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import classes from './styles.module.scss';
 import {ReactComponent as CoinIcon} from "../../../assets/images/coin-icon.svg";
 
-const BuyNowBtn = ({link}) => {
+const BuyNowBtn = ({onClick}) => {
     return (
-        <button className={classes.button}>
-            <a href={link} target={'_blank'} rel="noopener noreferrer">
-                <span className={classes.text}>Buy Now</span>
-                <CoinIcon/>
-            </a>
+        <button className={classes.button} onClick={onClick}>
+            <span className={classes.text}>Buy Now</span>
+            <CoinIcon/>
         </button>
     );
 };
--- src/pages/Home/liveChartSection/index.jsx
+++ src/pages/Home/liveChartSection/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import Modal from 'react-modal';
 import classes from './styles.module.scss';
 import SectionHeading from "./sectionHeading";
 import NumbersBlock from "./numbersBlock";
@@ -6,7 +7,37 @@ import {BuyBtn} from "components";
 import ChartBlock from "./chartBlock";
 import rockChart from "../../../assets/images/rock-chart.png";
 
+const paymentSettings = {
+    apiKey: '0a2236fc-af87-4939-bb34-9eb3f0280e77',
+    colors: {
+        main: '#453fd6',
+        background: '#100f0f'
+    }
+};
+
+const paymentStyles = {
+    content: {
+        width: 'fit-content',
+        height: 'fit-content'
+    },
+};
+
 const LiveChartSection = () => {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    const ref = useRef();
+
+    const openModal = () => setIsOpen(true);
+
+    const closeModal = () => setIsOpen(false);
+
+    const openPaymentForm = async () => {
+        await openModal();
+
+        const widget = new window.SwipeluxWidget(ref.current, paymentSettings);
+        widget.init()
+    }
+
     return (
         <div className={classes.containerPage}>
             <section className={classes.wrapper}>
@@ -15,9 +46,18 @@ const LiveChartSection = () => {
                         <SectionHeading/>
                         <NumbersBlock/>
                         <div className={classes.btnWrapper}>
-                            <BuyBtn link={'https://www.xt.com/trade/qie_usdt'}/>
-                            {/*<div className={classes.text}>Available on XT, bitmart  digifinex and 55 more</div>*/}
+                            <BuyBtn onClick={openPaymentForm}/>
+                            <Modal
+                                isOpen={modalIsOpen}
+                                onRequestClose={closeModal}
+                                contentLabel="Example Modal"
+                                style={paymentStyles}
+                                className='paymentModal'
+                            >
+                                <div ref={ref} className={classes.payment}/>
+                            </Modal>
                         </div>
+
                     </div>
                     <div className={classes.chartBlockWrapper}>
                         <ChartBlock/>
@@ -26,20 +66,24 @@ const LiveChartSection = () => {
                 <div className={classes.footerWrapper}>
                     Buy on
                     &nbsp;
-                    <a className={classes.link} href={'https://p2pb2b.com/trade/QIE_USDT/'} target={'_blank'} rel="noopener noreferrer">
+                    <a className={classes.link} href={'https://p2pb2b.com/trade/QIE_USDT/'} target={'_blank'}
+                       rel="noopener noreferrer">
                         p2pb2b
                     </a>
                     ,
                     &nbsp;
-                    <a className={classes.link} href={'https://www.digifinex.com/en-ww/trade/USDT/QIE'} target={'_blank'} rel="noopener noreferrer">
+                    <a className={classes.link} href={'https://www.digifinex.com/en-ww/trade/USDT/QIE'}
+                       target={'_blank'} rel="noopener noreferrer">
                         digifinex
                     </a>,
                     &nbsp;
-                    <a target={'_blank'} className={classes.link} href={'https://www.bitmart.com/trade/en?symbol=QIE_USDT&layout=basic'} rel="noopener noreferrer">
+                    <a target={'_blank'} className={classes.link}
+                       href={'https://www.bitmart.com/trade/en?symbol=QIE_USDT&layout=basic'} rel="noopener noreferrer">
                         bitmart
                     </a>,
                     &nbsp;
-                    <a className={classes.link} href={'https://www.xt.com/trade/qie_usdt'} target={'_blank'} rel="noopener noreferrer">
+                    <a className={classes.link} href={'https://www.xt.com/trade/qie_usdt'} target={'_blank'}
+                       rel="noopener noreferrer">
                         xt.com
                     </a>
                 </div>
